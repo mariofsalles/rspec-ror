44573183c6e8a6a6ae4a350b147ef26ec63f73e9
Mario Salles <mariofsalles@gmail.com>
2019-02-19 17:30:45 +0000

    Create list

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it 'Have one order' do
-    customer = create(:customer)
-    order = create(:order, customer: customer)
+  it '# have one order' do
+    order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it '# have three orders' do
+    orders = create_list(:order, 3)
+    expect(orders.count).to eq(3)
+  end
+
 end
