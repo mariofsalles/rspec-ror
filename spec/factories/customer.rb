f91190b31b05ee9342fc2e246e3848684924b559
Mario Salles <mariofsalles@gmail.com>
2019-02-19 11:01:42 +0000

    Inheritance, attributes_for, transient attributes

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,6 +1,26 @@
 FactoryBot.define do
   factory :customer, aliases:[:user] do
+
+    transient do
+      upcased false
+    end
+
     name Faker::Name.name
     email Faker::Internet.email
+
+    factory :customer_vip do
+      vip true
+      days_to_pay 30
+    end
+
+    factory :customer_default do
+      vip false
+      days_to_pay 15
+    end
+
+    after(:create) do |customer, evaluator|
+      customer.name.upcase! if evaluator.upcased
+    end
+    
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -7,9 +7,31 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to eq("Sr. Mario Salles")
   end
 
+  it '# full_name - customer vip' do
+    customer = create(:customer_vip)
+    expect(customer.vip).to eq(true)
+  end
+
+  it '# full_name - customer default' do
+    customer = create(:customer_default)
+    expect(customer.days_to_pay).to eq(15)
+  end
+
   it '# full_name' do
     customer = create(:user) # or :customer, because :user is a alias in this factory
     expect(customer.full_name).to start_with("Sr. ")
+  end
+
+  it '# using attributes_for' do
+    attrs = attributes_for(:customer)
+    customer = Customer.create(attrs)
+    expect(customer.full_name).to start_with("Sr.")
+  end
+  
+  it '# using transient attributes' do
+    customer = create(:customer, upcased: true)
+    # puts attributes_for(:customer_default)
+    expect(customer.name.upcase).to eq(customer.name)
   end
 
   it { expect{create(:customer)}.to change{Customer.all.size}.by 1 }
