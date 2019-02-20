4a13605abfbd2ed7f03e20563099fe4c6a2f3461
Mario Salles <mariofsalles@gmail.com>
2019-02-20 20:13:07 +0000

    VCR with metadates of RSpec

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,13 +1,9 @@
 describe 'HTTParty' do
-  it 'content-type' do
-
-
+  it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts'} do # vide o porque do simbolo na linha 6
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/3").to_return(status: 200, body: "", headers: {'content-type':'application/json'})
-      VCR.use_cassette("jsonplaceholder/posts") do
-        response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/3')
-        content_type = response.headers['content-type']
-        expect(content_type).to match(/application\/json/)
-      end
+    # VCR.use_cassette("jsonplaceholder/posts") do  --------> NÃO É MAIS NECESSÁRIO COM A INSERÇÃO DO         config.configure_rspec_metadata!
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/3')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
   end
-
 end
