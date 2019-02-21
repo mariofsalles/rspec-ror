40a89cbd619dae67320158c065a1c33b7d22e1d3
Mario Salles <mariofsalles@gmail.com>
2019-02-21 02:23:14 +0000

    Record methods

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,8 +1,10 @@
 describe 'HTTParty' do
-  it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts'} do # vide o porque do simbolo na linha 6
+  #it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do # vide o porque do simbolo na linha 7
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', :record => :new_episodes } do 
+
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/3").to_return(status: 200, body: "", headers: {'content-type':'application/json'})
     # VCR.use_cassette("jsonplaceholder/posts") do  --------> NÃO É MAIS NECESSÁRIO COM A INSERÇÃO DO         config.configure_rspec_metadata!
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/3')
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4,5,6].sample}")
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
