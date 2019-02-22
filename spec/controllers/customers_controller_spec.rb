d3a92a99b7b160881ce3ca33c04d97bd72adfd8e
Mario Salles <mariofsalles@gmail.com>
2019-02-22 15:11:57 +0000

    Testing flash notice

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -27,6 +27,13 @@ RSpec.describe CustomersController, type: :controller do
       @member = create(:member)
       @customer = create(:customer)
     end
+
+    it 'Flash Notice' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      post :create, params: {customer: customer_params}
+      expect(flash[:notice]).to match(/successfully created/)
+    end
       
     it 'with valid attributes' do
       customer_params = attributes_for(:customer)
@@ -40,7 +47,7 @@ RSpec.describe CustomersController, type: :controller do
       expect(response).to have_http_status '200'
     end
 
-    it 'render a :show template' do
+    it 'render :show template' do
       sign_in @member
       get :show, params: {id: @customer.id}
       expect(response).to render_template(:show)
