8a1470c5c388957169d8dea1e4b7237b0e5f88f3
Mario Salles <mariofsalles@gmail.com>
2019-02-22 12:05:31 +0000

    Controller testing with devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
+require 'rspec/rails'
+require 'devise'
 require File.expand_path('../../config/environment', __FILE__)
+
+
+ENV['RAILS_ENV'] ||= 'test'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -40,6 +43,9 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
+
+  # Devise
+  config.include Devise::Test::ControllerHelpers, :type => :controller
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
