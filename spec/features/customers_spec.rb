ac1129d82d50594644bf69cc708b2c8b01b56c1a
Mario Salles <mariofsalles@gmail.com>
2019-02-22 19:41:44 +0000

    Testing views with Capybara

---
+++ spec/features/customers_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature "Customers", type: :feature do
+  it 'Visit index page' do
+    visit(customers_path)
+    save_and_open_page
+    expect(page).to have_current_path(customers_path)
+  end
+end
